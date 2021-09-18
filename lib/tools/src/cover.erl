63ef6a602837e53f56205c1f4bda26a1ea811de2
Fred Hebert <mononcqc@ferd.ca>
2021-09-18 08:42:24 -0400

    Do the easy {block,_,_,_} support bits
    Adds the basic forms to erl_syntax, parse transforms, etc.
    
    Ignores the trickier bits, namely:
    - erl_lint, which we hope to avoid for the parse transform phase
    - erl_eval, which may reuse a parse transform's code
    - v3_core, which would have a final non-ptrans handling as well.

--- lib/tools/src/cover.erl
+++ lib/tools/src/cover.erl
@@ -162,9 +162,9 @@
 -define(SERVER, cover_server).
 
 %% Line doesn't matter.
--define(BLOCK(Expr), {block,erl_anno:new(0),[Expr]}).
--define(BLOCK1(Expr), 
-        if 
+-define(BLOCK(Expr), {block,erl_anno:new(0),[Expr],[]}).
+-define(BLOCK1(Expr),
+        if
             element(1, Expr) =:= block ->
                 Expr;
             true -> ?BLOCK(Expr)
@@ -2233,9 +2233,10 @@ munge_expr({bc,Anno,Expr,Qs}, Vars) ->
     {MungedExpr,Vars2} = munge_expr(?BLOCK1(Expr), Vars),
     {MungedQs, Vars3} = munge_qualifiers(Qs, Vars2),
     {{bc,Anno,MungedExpr,MungedQs}, Vars3};
-munge_expr({block,Anno,Body}, Vars) ->
+munge_expr({block,Anno,Body,Clauses}, Vars) ->
     {MungedBody, Vars2} = munge_body(Body, Vars),
-    {{block,Anno,MungedBody}, Vars2};
+    {MungedClauses, Vars3} = munge_clauses(Clauses, Vars2),
+    {{block,Anno,MungedBody,MungedClauses}, Vars2};
 munge_expr({'if',Anno,Clauses}, Vars) ->
     {MungedClauses,Vars2} = munge_clauses(Clauses, Vars),
     {{'if',Anno,MungedClauses}, Vars2};
